d24e5ab0b16d67b0ae8914ff564de9da91e002ea
Volodymyr <vmsemchyshyn@gmail.com>
2020-08-27 20:39:10 +0300

    getSomeCommits, createPatch, apply

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,53 +1,95 @@
 package com.company;
 
-
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
-
 import java.io.*;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+
+
 
 public class Main {
 
-    public static void runProcess(Process process,StringBuilder builder) throws IOException {
+    public static void runProcess(Process process, StringBuilder builder) throws IOException {
         BufferedReader pReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line = null;
-        while((line=pReader.readLine())!=null){
+        while ((line = pReader.readLine()) != null) {
             builder.append(line);
         }
-        String result = builder.toString();
-        System.out.print(result);
+        System.out.println(builder);
     }
 
-    public static void getCommits(String command) throws IOException {
-        StringBuilder builder = new StringBuilder();
+    public static StringBuilder getAllCommits(String command) throws IOException {
+        StringBuilder commits = new StringBuilder();
         Process process = Runtime.getRuntime().exec(command);
-        runProcess(process,builder);
+        runProcess(process, commits);
+        return commits;
     }
 
-    public static void getCommitByHash(String command,String hashCode) throws IOException {
-        StringBuilder builder = new StringBuilder();
-        Process process = Runtime.getRuntime().exec(command+hashCode);
+    public static StringBuilder getCommit(String command, String hashCode) throws IOException {
+        StringBuilder commit = new StringBuilder();
+        Process process = Runtime.getRuntime().exec(command + " " + hashCode);
+        runProcess(process, commit);
+        return commit;
+    }
 
-        runProcess(process, builder);
+    public static String[] getCommitsHash(StringBuilder commits) {
+        commits.replace(0,1," ");
+        commits.replace(commits.length()-1,commits.length()," ");
+        for (int i = 0; i < commits.length(); i++) {
+            if (commits.charAt(i) == '"' && commits.charAt(i + 1) == '"') {
+                commits.replace(i, i + 2, " ");
+            }
+        }
+
+        String newArray=new String(commits);
+        String[] splitedCommits=newArray.split(" ");
+
+        for (int i = 0; i < splitedCommits.length; i++) {
+            System.out.println(splitedCommits[i]+" ");
+        }
+           return splitedCommits;
+    }
+
+    //        git rev-list 9b18c99 ^9e26fd4^ ^9b18c99^
+    public static void chooseSomeCommits(String from,String to) throws IOException{
+        StringBuilder commit = new StringBuilder();
+        Process process = Runtime.getRuntime().exec("git rev-list "+ from+"^ ^"+to+ "^ ");
+        runProcess(process,commit);
+    }
+
+
+    public static void creatPatch(String[] commits,String fileName) throws IOException {
+        StringBuilder commit = new StringBuilder();
+        Process process = Runtime.getRuntime().exec("git diff "+ commits[0]+" "+ commits[1]+
+                "> "+ fileName+".patch");
+        runProcess(process, commit);
+    }
+
+    public static void applyPatch(String filename) throws IOException {
+        StringBuilder commit = new StringBuilder();
+        Process process = Runtime.getRuntime().exec("git apply "+filename+ ".patch");
+        runProcess(process, commit);
     }
 
 
 
-    public static void main(String[] args) throws IOException, GitAPIException, InterruptedException {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Git log: ");
+        getAllCommits("git log");
+        System.out.println("Get commit by hash");
+        System.out.println(getCommit("git show ","9e26fd451b3a8800a91d8147f9384a7a7c9f6ac6"));
+
+        System.out.println("Format: ");
+        getAllCommits("git log --pretty=format:\"%h\"");
 
 
-        getCommits("git log");
-        System.out.println();
-        getCommitByHash("git show ","9e26fd451b3a8800a91d8147f9384a7a7c9f6ac6");
+        System.out.println("Get commits hash: ");
+        System.out.println( getCommitsHash(getAllCommits("git log --pretty=format:\"%h\"")));
+
+        System.out.println("Choose some commits: ");
+        chooseSomeCommits("9b18c99","9e26fd4");
+
+        System.out.println("Create and apply: ");
+        creatPatch(getCommitsHash(getAllCommits("git log --pretty=format:\"%h\"")), "myfile");
+        applyPatch("myFile");
 
 
     }
